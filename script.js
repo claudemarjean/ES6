d465f9d031974f02d03aa22b3a971d8e525405bf
claudemarjean <marjean.8888@gmail.com>
2023-03-22 20:58:35 +0100

    paramètres spread 2

--- script.js
+++ script.js
@@ -4,19 +4,26 @@
  * L'opérateur REST prend au paramètre des valeur libre et transforme en tableau
  * la syntaxe dans la référence de ce deux est identique mais c'est le contenue de donné qui est envoyé qui détermine que c'est une opérateur REST ou Spread
  */
-const mesNombres = [40,5,14];
-
-function makeMeSum(nombre1,nombre2,nombre3){
-    return nombre1 + nombre2 + nombre3;
-}
-
-console.log(makeMeSum(...mesNombres));
 
 /**
- * Paramètre Spread d'ES6
+ * l'objet est passer par reférence
+ */
+const fruitsRouges = ["fraise", "framboise"];
+const fruitRouges2 = fruitsRouges;
+
+fruitRouges2.push("cerises");
+
+console.log(fruitsRouges);
+console.log(fruitRouges2);
+
+/**
+ * avec le paramètres spread on peut faire comme ceci
  */
 
+const fruitsRougesSpread = ["fraise", "framboise"];
+const fruitRouges2Spread = [...fruitsRouges];
 
-const makeMeSumRest = (nombre1,nombre2,nombre3) => nombre1 + nombre2 + nombre3
+fruitRouges2Spread.push("cerises");
 
-console.log(makeMeSumRest(...mesNombres));
+console.log(fruitsRougesSpread);
+console.log(fruitRouges2Spread);
