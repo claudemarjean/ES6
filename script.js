f60968d0a29447e4535e9e06e70b187ce2739d12
claudemarjean <marjean.8888@gmail.com>
2023-03-22 21:15:54 +0100

    interpolation / templates literals

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-const fruitsRouges = ["Fraise","framboise", "cerise"];
+let valeur = "valeur 5 de val"
 
-for (let i = 0; i < fruitsRouges.length ; i++){
-    console.log(`j'aime bien le ${fruitsRouges[i]} :D`);
-}
+const stringMultiLigne = ` Je suis sur la ligne 1
+${valeur}
+je suis sur la ligne 3
+je suis sur la ligne 4
+`
 
-
-console.log("#################")
-/**
- * pour la syntaxe for of c'est comme ça
- */
-
-
-for (let fruit of  fruitsRouges){
-    console.log(`j'aime bien le ${fruit} :D`);
-}
-// c'est simple n'est ce pas? :D
+console.log(stringMultiLigne);
