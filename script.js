28e0edd44b339e15541d528c6e1c190c5a2ff305
claudemarjean <marjean.8888@gmail.com>
2023-03-22 20:53:09 +0100

    opérateur Spread

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-const mesNombres = [40,5,14,5,7,8];
+/**
+ * l'opérateur Spread 
+ * C'est le sens inverse de l'opérateur REST (il va prend au paramètre un tableau éclate)
+ * L'opérateur REST prend au paramètre des valeur libre et transforme en tableau
+ * la syntaxe dans la référence de ce deux est identique mais c'est le contenue de donné qui est envoyé qui détermine que c'est une opérateur REST ou Spread
+ */
+const mesNombres = [40,5,14];
 
-function makeMeSum(nombres){
-    let sum = 0;
-    for (let i = 0; i < nombres.length ; i++){
-        sum += nombres[i];
-    }
-    return sum;
+function makeMeSum(nombre1,nombre2,nombre3){
+    return nombre1 + nombre2 + nombre3;
 }
 
-console.log(makeMeSum(mesNombres));
+console.log(makeMeSum(...mesNombres));
 
 /**
- * Paramètre Rest d'ES6
- * Prend la valeur libre "..." avant la variable
+ * Paramètre Spread d'ES6
  */
 
 
-const makeMeSumRest = (...nombres) => {
-    let sum = 0;
-    for (let i = 0; i < nombres.length ; i++){
-        sum += nombres[i];
-    }
-    return sum;
-}
+const makeMeSumRest = (nombre1,nombre2,nombre3) => nombre1 + nombre2 + nombre3
 
-console.log(makeMeSumRest(40,5,14,5,7,8));
+console.log(makeMeSumRest(...mesNombres));
