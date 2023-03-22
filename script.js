733852b3a108f5fbc1d2856edcdf77dfd77ae74a
claudemarjean <marjean.8888@gmail.com>
2023-03-22 21:34:36 +0100

    destructuring Arrays

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const tableau = [1,2,5];
+console.log(tableau);
+
 
 const d = tableau[0];
 const e = tableau[1];
@@ -18,3 +20,21 @@ const [a,b] = nombres;
 
 console.log(a);
 console.log(b);
+
+console.log("###############");
+/**
+ * Avec le paramètre REST
+ */
+
+const [x,...y] = nombres; //y prend les 2 valeur libre de nombre 2,5 et il va mettre dans un tableau
+
+console.log(x);
+console.log(y);//[2,5]
+
+console.log("###############");
+/**
+ * et si je veux sauter la valeur 2 je peux faire comme ceci
+ */
+const[g,,h] = nombres;
+console.log(g);
+console.log(h);
